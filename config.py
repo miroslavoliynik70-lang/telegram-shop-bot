839dd195bddee022c9ebd0af934130a368744824
miroslavoliynik70-lang <miroslavoliynik70@gmail.com>
2026-01-29 13:23:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-BOT_TOKEN = "8573209901:AAHhgoWdYpMlh3Wf2h-Qm5V4qVp9eTJegME"
-ADMIN_IDS = [5370573727, 6180039889]
-CURRENCY = "EUR"
+import os
 
+BOT_TOKEN = os.getenv("8573209901:AAHhgoWdYpMlh3Wf2h-Qm5V4qVp9eTJegME")
+
+_raw = os.getenv("5370573727", "6180039889")
+ADMIN_IDS = {int(x) for x in _raw.split(",") if x.strip().isdigit()}
 
