3562c89bdd1f83b4b76130fc2c9e9292ecdd6a98
miroslavoliynik70-lang <miroslavoliynik70@gmail.com>
2026-01-29 14:31:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ BOT_TOKEN = os.getenv("8573209901:AAHhgoWdYpMlh3Wf2h-Qm5V4qVp9eTJegME")
 _raw = os.getenv("5370573727", "6180039889")
 ADMIN_IDS = {int(x) for x in _raw.split(",") if x.strip().isdigit()}
 
+CURRENCY = "RUB"
