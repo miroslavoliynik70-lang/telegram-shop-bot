293c367e2a9ea2c38a582f888fa1bb661a65fd67
miroslavoliynik70-lang <miroslavoliynik70@gmail.com>
2026-01-29 14:34:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("8573209901:AAHhgoWdYpMlh3Wf2h-Qm5V4qVp9eTJegME")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 _raw = os.getenv("5370573727", "6180039889")
 ADMIN_IDS = {int(x) for x in _raw.split(",") if x.strip().isdigit()}
